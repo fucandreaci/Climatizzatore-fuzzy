965c88a9a9a259b6025329579ed96440490160b4
Andrea Fucci <fucciandrea01@gmail.com>
2023-04-16 17:19:10 +0200

    Added requirements ad code improvements

--- climatizzatore.py
+++ climatizzatore.py
@@ -4,22 +4,16 @@ from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
 # Definizione delle variabili linguistiche per i due input e l'output
-temperatura_esterna = ctrl.Antecedent(
-    np.arange(-10, 41, 1), 'temperatura_esterna')
+temperatura_esterna = ctrl.Antecedent(np.arange(-10, 41, 1), 'temperatura_esterna')
 persone = ctrl.Antecedent(np.arange(0, 11, 1), 'persone')
 stanza = ctrl.Consequent(np.arange(-10, 31, 1), 'stanza')
 
 # Definizione delle funzioni di appartenenza per le variabili linguistiche
-temperatura_esterna['molto freddo'] = fuzz.trapmf(
-    temperatura_esterna.universe, [-10, -10, 5, 10])
-temperatura_esterna['freddo'] = fuzz.trimf(
-    temperatura_esterna.universe, [5, 10, 20])
-temperatura_esterna['moderato'] = fuzz.trimf(
-    temperatura_esterna.universe, [15, 20, 25])
-temperatura_esterna['caldo'] = fuzz.trimf(
-    temperatura_esterna.universe, [20, 25, 35])
-temperatura_esterna['molto caldo'] = fuzz.trapmf(
-    temperatura_esterna.universe, [25, 35, 40, 40])
+temperatura_esterna['molto freddo'] = fuzz.trapmf(temperatura_esterna.universe, [-10, -10, 5, 10])
+temperatura_esterna['freddo'] = fuzz.trimf(temperatura_esterna.universe, [5, 10, 20])
+temperatura_esterna['moderato'] = fuzz.trimf(temperatura_esterna.universe, [15, 20, 25])
+temperatura_esterna['caldo'] = fuzz.trimf(temperatura_esterna.universe, [20, 25, 35])
+temperatura_esterna['molto caldo'] = fuzz.trapmf(temperatura_esterna.universe, [25, 35, 40, 40])
 
 persone['poche'] = fuzz.trapmf(persone.universe, [0, 0, 2, 5])
 persone['normali'] = fuzz.trimf(persone.universe, [3, 5, 8])
@@ -30,64 +24,51 @@ stanza['normale'] = fuzz.trimf(stanza.universe, [10, 15, 25])
 stanza['calda'] = fuzz.trimf(stanza.universe, [20, 30, 30])
 
 # Definizione delle regole fuzzy
-# Regole per il caso in cui le persone sono poche
-rule1 = ctrl.Rule(
-    temperatura_esterna['molto freddo'] & persone['poche'], stanza['fredda'])
-rule2 = ctrl.Rule(temperatura_esterna['freddo']
-                  & persone['poche'], stanza['fredda'])
-rule3 = ctrl.Rule(
-    temperatura_esterna['moderato'] & persone['poche'], stanza['normale'])
-rule4 = ctrl.Rule(temperatura_esterna['caldo']
-                  & persone['poche'], stanza['calda'])
-rule5 = ctrl.Rule(
-    temperatura_esterna['molto caldo'] & persone['poche'], stanza['calda'])
+# Regole per il caso in cui le persone sono di tipo "poche"
+rule1 = ctrl.Rule(temperatura_esterna['molto freddo'] & persone['poche'], stanza['fredda'])
+rule2 = ctrl.Rule(temperatura_esterna['freddo'] & persone['poche'], stanza['fredda'])
+rule3 = ctrl.Rule(temperatura_esterna['moderato'] & persone['poche'], stanza['normale'])
+rule4 = ctrl.Rule(temperatura_esterna['caldo'] & persone['poche'], stanza['calda'])
+rule5 = ctrl.Rule(temperatura_esterna['molto caldo'] & persone['poche'], stanza['calda'])
 
-# Regole per il caso in cui le persone sono normali
-rule6 = ctrl.Rule(
-    temperatura_esterna['molto freddo'] & persone['normali'], stanza['fredda'])
-rule7 = ctrl.Rule(temperatura_esterna['freddo']
-                  & persone['normali'], stanza['normale'])
-rule8 = ctrl.Rule(
-    temperatura_esterna['moderato'] & persone['normali'], stanza['normale'])
-rule9 = ctrl.Rule(temperatura_esterna['caldo']
-                  & persone['normali'], stanza['calda'])
-rule10 = ctrl.Rule(
-    temperatura_esterna['molto caldo'] & persone['normali'], stanza['calda'])
+# Regole per il caso in cui le persone sono di tipo "normali"
+rule6 = ctrl.Rule(temperatura_esterna['molto freddo'] & persone['normali'], stanza['fredda'])
+rule7 = ctrl.Rule(temperatura_esterna['freddo'] & persone['normali'], stanza['normale'])
+rule8 = ctrl.Rule(temperatura_esterna['moderato'] & persone['normali'], stanza['normale'])
+rule9 = ctrl.Rule(temperatura_esterna['caldo'] & persone['normali'], stanza['calda'])
+rule10 = ctrl.Rule(temperatura_esterna['molto caldo'] & persone['normali'], stanza['calda'])
 
-# Regole per il caso in cui le persone sono molte
-rule11 = ctrl.Rule(
-    temperatura_esterna['molto freddo'] & persone['molte'], stanza['fredda'])
-rule12 = ctrl.Rule(
-    temperatura_esterna['freddo'] & persone['molte'], stanza['normale'])
-rule13 = ctrl.Rule(
-    temperatura_esterna['moderato'] & persone['molte'], stanza['calda'])
-rule14 = ctrl.Rule(
-    temperatura_esterna['caldo'] & persone['molte'], stanza['calda'])
-rule15 = ctrl.Rule(
-    temperatura_esterna['molto caldo'] & persone['molte'], stanza['calda'])
+# Regole per il caso in cui le persone sono di tipo "molte"
+rule11 = ctrl.Rule(temperatura_esterna['molto freddo'] & persone['molte'], stanza['fredda'])
+rule12 = ctrl.Rule(temperatura_esterna['freddo'] & persone['molte'], stanza['normale'])
+rule13 = ctrl.Rule(temperatura_esterna['moderato'] & persone['molte'], stanza['calda'])
+rule14 = ctrl.Rule(temperatura_esterna['caldo'] & persone['molte'], stanza['calda'])
+rule15 = ctrl.Rule(temperatura_esterna['molto caldo'] & persone['molte'], stanza['calda'])
 
-stanza_ctrl = ctrl.ControlSystem(
-    [rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule1, rule2, rule3, rule4, rule5])
+stanza_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13, rule14, rule15])
 
 stanza_sim = ctrl.ControlSystemSimulation(stanza_ctrl)
 
-inputTemp = 20
-inputPersone = 8
+# Inserimento dei valori di input
+inputTemp = int(input("Inserisci la temperatura della stanza (°C): "))
+inputPersone = int(input("Inserisci il numero di persone nella stanza: "))
 
 stanza_sim.input['temperatura_esterna'] = inputTemp
 stanza_sim.input['persone'] = inputPersone
 
 stanza_sim.compute()
 
+# Stampa dei risultati
 print("Temperatura esterna:", inputTemp)
 print("Numero di persone nella stanza:", inputPersone)
-print("Temperatura della stanza:", stanza_sim.output['stanza'])
+print("Temperatura della stanza:", round(stanza_sim.output['stanza']), "°C")
 
+# Visualizzazione dei grafici
 temperatura_esterna.view()
-plt.savefig("temperatura.png")
+plt.savefig("grafico-temperatura.png")
 
 persone.view()
-plt.savefig("persone.png")
+plt.savefig("grafico-persone.png")
 
 stanza.view()
-plt.savefig("stanza.png")
+plt.savefig("grafico-stanza.png")
